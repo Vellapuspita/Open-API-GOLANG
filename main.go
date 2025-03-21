21f72a744e38cd541cbe0a57edc199b81abec944
Vella Puspita <vellapuspita0@gmail.com>
2025-03-21 10:31:50 +0800

    install dwiver mysql gorm

--- main.go
+++ main.go
@@ -1,14 +1,15 @@
-package main 
+package main
+
 import (
 	"fmt"
 	"net/http"
 )
 
-fun handler(w http.ResponseWriter, r *http.Request){
+func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3000", nil)
 }
