6d461aebb123530db94daab09394bc64433dce35
Vella Puspita <vellapuspita0@gmail.com>
2025-03-21 10:52:51 +0800

    melengkapi endpoint

--- main.go
+++ main.go
@@ -19,13 +19,16 @@ type User struct {
 	UpdatedAt time.Time `gorm:"column:updated_at"`
 }
 
+func (User) TableName() string {
+	return "user"
+}
+
 func main() {
 	dsn := "root:@tcp(localhost:3306)/openapi?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		fmt.Println("failed to Conenct database")
+		fmt.Println("Tidak berhasil konek ke database")
 	}
-
 	router := gin.Default()
 
 	router.GET("/users", func(c *gin.Context) {
@@ -34,6 +37,61 @@ func main() {
 		c.JSON(http.StatusOK, gin.H{"data": users})
 	})
 
-	router.Run(":3000")
+	router.POST("/users", func(c *gin.Context) {
+		var newUsers User
 
+		if err := c.ShouldBindJSON(&newUsers); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		db.Create(&newUsers)
+		c.JSON(http.StatusCreated, gin.H{"data": newUsers})
+	})
+
+	router.GET("/users/:id", func(c *gin.Context) {
+		var user User
+		id := c.Param("id")
+
+		if err := db.First(&user, id).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User tidak ditemukan"})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{"data": user})
+	})
+
+	router.PUT("/users/:id", func(c *gin.Context) {
+		var user User
+		id := c.Param("id")
+
+		if err := db.First(&user, id).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User tidak ditemukan"})
+			return
+		}
+
+		if err := c.ShouldBindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		db.Save(&user)
+
+		c.JSON(http.StatusOK, gin.H{"message": "User berhasil diperbarui", "data": user})
+	})
+
+	router.DELETE("/users/:id", func(c *gin.Context) {
+		var user User
+		id := c.Param("id")
+
+		if err := db.First(&user, id).Error; err != nil {
+			c.JSON(http.StatusNotFound, gin.H{"error": "User tidak ditemukan"})
+			return
+		}
+
+		db.Delete(&user)
+
+		c.JSON(http.StatusOK, gin.H{"message": "User berhasil dihapus"})
+	})
+
+	router.Run(":3000")
 }
